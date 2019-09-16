a6a21d8eb33bde396276eccfa39f5001269c87db
quanvo <quan.vo@saigontechnology.com>
2019-09-16 10:57:08 +0700

    import PropTypes, adjust resizeMode

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-import React, { Component, PropTypes } from 'react'
-import { Animated, View, Image, StyleSheet } from 'react-native'
+import React, {Component} from "react";
+import {Animated, View, Image, StyleSheet} from "react-native";
+import PropTypes from "prop-types";
+
 
 export default class ProgressiveImage extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       imageOpacity: new Animated.Value(0),
-      thumbnailOpacity: new Animated.Value(0),
-    }
+      thumbnailOpacity: new Animated.Value(0)
+    };
   }
 
   onLoadThumbnail() {
     Animated.timing(this.state.thumbnailOpacity, {
       toValue: 1,
-      duration: this.props.thumbnailFadeDuration,
-    }).start()
-    this.props.onLoadThumbnail()
+      duration: this.props.thumbnailFadeDuration
+    }).start();
+    this.props.onLoadThumbnail();
   }
 
   onLoadImage() {
     Animated.timing(this.state.imageOpacity, {
       toValue: 1,
-      duration: this.props.imageFadeDuration,
-    }).start()
-    this.props.onLoadImage()
+      duration: this.props.imageFadeDuration
+    }).start();
+    this.props.onLoadImage();
   }
 
   render() {
@@ -32,35 +34,36 @@ export default class ProgressiveImage extends Component {
         <Image
           resizeMode="cover"
           style={[styles.image, this.props.style]}
-          source={this.props.placeHolderSource}
+          source={this.props.placeHolderSource || this.props.thumbnailSource}
+          blurRadius={this.props.thumbnailBlurRadius}
         />
         <Animated.Image
           resizeMode="cover"
-          style={[styles.image, { opacity: this.state.thumbnailOpacity }, this.props.style]}
+          style={[styles.image, {opacity: this.state.thumbnailOpacity}, this.props.style]}
           source={this.props.thumbnailSource}
           onLoad={() => this.onLoadThumbnail()}
           blurRadius={this.props.thumbnailBlurRadius}
         />
         <Animated.Image
-          resizeMode="cover"
-          style={[styles.image, { opacity: this.state.imageOpacity }, this.props.style]}
+          resizeMode={this.props.resizeMode}
+          style={[styles.image, {opacity: this.state.imageOpacity}, this.props.style]}
           source={this.props.imageSource}
           onLoad={() => this.onLoadImage()}
         />
       </View>
-    )
+    );
   }
 }
 
- const styles = StyleSheet.create({
-   image: {
-     position: 'absolute',
-     top: 0,
-     bottom: 0,
-     left: 0,
-     right: 0,
-   },
- })
+const styles = StyleSheet.create({
+  image: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  }
+});
 
 ProgressiveImage.propTypes = {
   placeHolderColor: PropTypes.string,
@@ -71,13 +74,13 @@ ProgressiveImage.propTypes = {
   onLoadImage: PropTypes.func.isRequired,
   thumbnailSource: PropTypes.object.isRequired,
   thumbnailFadeDuration: PropTypes.number.isRequired,
-  thumbnailBlurRadius: PropTypes.number,
-}
+  thumbnailBlurRadius: PropTypes.number
+};
 
 ProgressiveImage.defaultProps = {
   thumbnailFadeDuration: 250,
   imageFadeDuration: 250,
   thumbnailBlurRadius: 5,
   onLoadThumbnail: Function.prototype,
-  onLoadImage: Function.prototype,
-}
+  onLoadImage: Function.prototype
+};
